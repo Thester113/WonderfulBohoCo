81429976becf9fa1d3c3317255b858805080bc1a
tomhester <tomhester113@gmail.com>
2022-02-20 13:38:22 -0700

    Clean Up

--- src/App.js
+++ src/App.js
@@ -9,13 +9,6 @@ function App() {
         <p>
           WonderfulBohoCo coming soon!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
       </header>
       <div className = "App-footer">
          <footer>
